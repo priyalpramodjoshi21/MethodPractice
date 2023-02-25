10ee751d48abfa35c5d5752ad62721148a835893
“priyalpramodjoshi21” <priyalpramodjoshi@gmail.com>
2023-02-25 00:09:38 -0800

    added methods

---
+++ src/Com/bridgelabz/Add.java
@@ -0,0 +1,16 @@
+package Com.bridgelabz;
+
+public class Add {
+
+	public static void v1() {
+		int a=2;
+		int b=4;
+		int sum= a + b;
+		System.out.println("sum is:     " + sum);
+		
+	}
+	
+public static void main(String[] args) {
+	  v1();
+}
+}
